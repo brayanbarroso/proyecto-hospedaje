991fed3ad21f5c3076ce7c946b38cf1d1fd30cc3
brayner25 <66150347+brayner25@users.noreply.github.com>
2020-06-28 23:22:02 -0500

    Haciendo el menu fijo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("hola mundo");
+var menu = document.querySelector(".menu-principal");
+console.log(menu);
+window.onscroll = function () {
+  if (window.scrollY >= 660) {
+    menu.classList.add("fixed");
+  } else {
+    menu.classList.remove("fixed");
+  }
+};
